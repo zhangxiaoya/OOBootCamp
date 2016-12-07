eaa5a8ef5a72cf665994cff140890d1725630267
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-12-07 23:00:25 +0800

    [yanan] :cn: refactor all code

---
+++ OOBootCamp/ParkingLot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OOBootCamp
+{
+    public class ParkingLot {
+        private const uint DefaultCapacity = 4;
+        private readonly IList<Car> carList;
+        private uint capacity;
+
+        public ParkingLot()
+        {
+            capacity = DefaultCapacity;
+            carList = new List<Car>();
+        }
+
+        public int Park(Car car)
+        {
+            if (capacity > 0)
+            {
+                carList.Add(car);
+                capacity = capacity - 1;
+                return car.GetHashCode();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Car Pick(int token)
+        {
+            foreach (var car in carList.Where(car => car.GetHashCode().Equals(token))) {
+                carList.Remove(car);
+                capacity++;
+                return car;
+            }
+            return null;
+        }
+
+        public IList<Car> UnAllPark()
+        {
+            capacity = DefaultCapacity;
+            return carList;
+        }
+
+        public uint RemianParkingSpace()
+        {
+            return capacity;
+        }
+    }
+}
