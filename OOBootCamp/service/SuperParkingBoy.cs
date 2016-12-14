3b35970ae4808d60caebb1f48f0f48dc8aed84e6
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-12-14 22:32:09 +0800

    [yanan] :yum: add first test

---
+++ OOBootCamp/service/SuperParkingBoy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using OOBootCamp.Model;
+
+namespace OOBootCamp.service
+{
+    public class SuperParkingBoy
+    {
+        private readonly IList<ParkingLot> parkingLots; 
+        public SuperParkingBoy(IList<ParkingLot> parkingLots)
+        {
+            this.parkingLots = parkingLots;
+        }
+
+        public int Park(Car car)
+        {
+            return parkingLots[0].Park(car);
+        }
+    }
+}
---
+++ OOBootCamp/test/SuperParkingBoyTest.cs
@@ -0,0 +1,28 @@
+﻿using OOBootCamp.Model;
+using OOBootCamp.service;
+using Xunit;
+
+namespace OOBootCamp.test
+{
+    public class SuperParkingBoyTest
+    {
+        [Fact]
+        public void should_park_one_car_to_parking_lot_which_have_high_cacancy_rate()
+        {
+            var parkingLotWithHighRate = new ParkingLot(4);
+            var parkingLotWithLowRate = new ParkingLot(3);
+            var superParkingBoy = new SuperParkingBoy(new []
+            {
+                parkingLotWithHighRate,
+                parkingLotWithLowRate
+            });
+            parkingLotWithHighRate.Park(new Car());
+            parkingLotWithLowRate.Park(new Car());
+            var carParingFirstTime = new Car();
+
+            var firstTimeParkingCarToken = superParkingBoy.Park(carParingFirstTime);
+
+            Assert.Same(carParingFirstTime,parkingLotWithHighRate.Pick(firstTimeParkingCarToken));
+        }
+    }
+}
