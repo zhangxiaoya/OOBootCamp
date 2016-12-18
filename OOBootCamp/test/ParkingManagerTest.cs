38cb695c807a40456841c742a2b5cd4ca8440b20
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-12-18 22:32:14 +0800

    [yanan] :cn: add test task

---
+++ OOBootCamp/test/ParkingManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace OOBootCamp.test
+{
+    public class ParkingManagerTest
+    {
+        [Fact]
+        public void should_ask_parking_boy_park_one_car_to_first_available_parking_lot()
+        {
+            
+        }
+
+        [Fact]
+        public void should_ask_smart_parking_boy_park_one_car_to_parking_lot_with_most_empty_parking_space()
+        {
+            
+        }
+
+        [Fact]
+        public void should_ask_super_parking_boy_park_one_car_to_parking_lot_with_highest_vacancy_rate()
+        {
+            
+        }
+
+        [Fact]
+        public void should_ask_any_parking_boy_pick_one_car_successfully()
+        {
+            
+        }
+
+        [Fact]
+        public void should_park_one_car_by_himself()
+        {
+            
+        }
+
+        [Fact]
+        public void should_park_car_failed_while_no_park_space_by_himself()
+        {
+            
+        }
+
+        [Fact]
+        public void should_pick_one_car_successfully_by_himself()
+        {
+            
+        }
+
+        [Fact]
+        public void should_pick_car_failed_by_himself()
+        {
+            
+        }
+    }
+}
